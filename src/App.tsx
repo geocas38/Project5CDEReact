997086a5a3964f715785287456a5f88a9efd3763
geocas38 <geovanicastro38@outlook.com>
2020-11-23 14:02:45 -0800

    updated starter files

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Overview</h1>
+          <section className="block1">
+              <h3>Updates</h3>
+              12/27/19 @11:51AM: Howard changed the CDE name to General Health Score. <br />
+              2/14/20 @7:13AM: Debby added to the discussion board. <br />
+              3/5/20 @1:28PM: Janet proposed adding a new column. <br />
+          </section>
+          <br />
+          <section className="block2">
+              <h3>Marriage Status</h3>
+              12/27/19 @11:51AM: Howard changed the CDE name to General Health Score. <br />
+              2/14/20 @7:13AM: Debby added to the discussion board. <br />
+              3/5/20 @1:28PM: Janet proposed adding a new column. <br />
+          </section>
+      </header>
+    </div>
+  );
+}
+
+export default App;
