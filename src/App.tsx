41ee0ef5cd252375f918005c825d62429da70be6
Sana Hakim <sanahakim@MacBook-Air.local>
2020-11-26 02:00:15 -0800

    Factored out code into separate methods

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,35 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Overview</h1>
-          <section className="block1">
-              <h3>Updates</h3>
-              12/27/19 @11:51AM: Howard changed the CDE name to General Health Score. <br />
-              2/14/20 @7:13AM: Debby added to the discussion board. <br />
-              3/5/20 @1:28PM: Janet proposed adding a new column. <br />
-          </section>
-          <br />
-          <section className="block2">
-              <h3>Marriage Status</h3>
-              12/27/19 @11:51AM: Howard changed the CDE name to General Health Score. <br />
-              2/14/20 @7:13AM: Debby added to the discussion board. <br />
-              3/5/20 @1:28PM: Janet proposed adding a new column. <br />
-          </section>
+          {Block1("Updates", "Updates")}
+          {Block2("Marriage Status", "Marriage Status")}
       </header>
     </div>
   );
+}
+
+function Block1(label: string, name: string) {
+  return (
+  <section className="block1">
+    <h3>{label}</h3>
+    <h2>{name}</h2>
+    12/27/19 @11:51AM: Howard changed the CDE name to General Health Score. <br />
+    2/14/20 @7:13AM: Debby added to the discussion board. <br />
+    3/5/20 @1:28PM: Janet proposed adding a new column. <br />
+  </section>
+  )
+}
+
+function Block2(label: string, name: string) {
+  return (
+  <section className="block2">
+    <h3>{label}</h3>
+    <h3>{name}</h3>
+    12/27/19 @11:51AM: Howard changed the CDE name to General Health Score. <br />
+    2/14/20 @7:13AM: Debby added to the discussion board. <br />
+    3/5/20 @1:28PM: Janet proposed adding a new column. <br />
+  </section>
+  )
 }
 
 export default App;
